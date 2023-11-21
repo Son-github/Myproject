ad58db9454070f8c9d6905de7bf8b76a06822104
jonginson <jongin718@gmail.com>
2023-11-21 15:01:01 +0900

    #4-밀키트 리스트 불러오기 기능 및 검색 기능 추가
    공통되게 들어가는 CreatedAt, ModifiedAt을 AuditingFields.java파일로 따로 뺌

---
+++ project_a/src/main/java/com/son/project_a/domain/AuditingFields.java
@@ -0,0 +1,31 @@
+package com.son.project_a.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditingFields {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @CreatedDate
+    @Column
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedAt;
+}
