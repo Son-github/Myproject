9fb22d751832fad6d46232e8d666e906d391eae1
jonginson <jongin718@gmail.com>
2023-11-23 00:02:38 +0900

    #5-페이징 기능 구현
    Repository 파일 수정

--- project_a/src/main/java/com/son/project_a/repository/MealKitRepository.java
+++ project_a/src/main/java/com/son/project_a/repository/MealKitRepository.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface MealKitRepository extends
         JpaRepository<MealKit, Long>,
         QuerydslPredicateExecutor<MealKit>,
