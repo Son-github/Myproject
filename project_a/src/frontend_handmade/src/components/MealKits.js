6f8676b0b663e6933ff9646ab46c48bbc2110367
jonginson <jongin718@gmail.com>
2023-11-27 23:27:55 +0900

    #5-페이징 기능 구현
    게시글 불러오기 기능 구현

--- project_a/src/frontend_handmade/src/components/MealKits.js
+++ project_a/src/frontend_handmade/src/components/MealKits.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 import {Box, CssVarsProvider} from "@mui/joy";
 import CssBaseline from "@mui/material/CssBaseline";
 import Stack from "@mui/joy/Stack";
@@ -9,35 +9,82 @@ import Filters from "./Filters";
 import RentalCard from "./RentalCard";
 import Pagination from "./Pagination";
 import axios from "axios";
+import MealKitService from "../MealKitService";
+import { useTable } from "react-table";
 
-export default function MealKits(){
+const MealKits = (props) => {
 
     const [mealKitsList, setMealKitsList] = useState([]);
+    const [searchMealKits, setSearchMealKits] = useState("");
+    const mealKitsRef = useRef();
 
+    const [page, setPage] = useState(1);
+    const [count, setCount] = useState(0);
+    const [pageSize, setPageSize] = useState(10);
 
-    useEffect( () => {
-        getMealKitsValue();
-    }, [])
+    const pageSizes = [5, 10, 15];
 
-    async function getMealKitsValue() {
-        await axios
-            .get('/mealKits')
+    mealKitsRef.current = mealKitsList;
+
+    const onChangeSearchMealKits = (e) => {
+        const searchMealKits = e.target.value;
+        setSearchMealKits(searchMealKits);
+    };
+
+    const getRequestParams = (searchMealKits, page, pageSize) => {
+        let params = {};
+
+        if (searchMealKits) {
+            params["mName"] = searchMealKits;
+        }
+
+        if (page) {
+            params["page"] = page - 1;
+        }
+
+        if (pageSize) {
+            params["size"] = pageSize;
+        }
+
+        return params;
+    };
+
+    const retrieveMealKits = () => {
+        const params = getRequestParams(searchMealKits, page, pageSize);
+
+        MealKitService.getMealKits(params)
             .then((res) => {
-                setMealKitsList(res.data);
-                console.log(res);
+                const { mealKits, totalPages} = res.data;
+
+                setMealKitsList(mealKits);
+                setCount(totalPages);
+
+                console.log("성공");
+                console.log(res.data);
             })
-            .catch((error) => {
-                console.log(error);
-                console.log("실패");
-            })
+            .catch( (e) => {
+                console.log(e);
+            });
+    };
+
+    useEffect( retrieveMealKits, [page, pageSize]);
+
+    console.log("Count: " + count);
+
+    const findByMealKitsName = () => {
+        setPage(1);
+        retrieveMealKits();
     }
 
-    console.log(mealKitsList.map( (res => {
-        return res.json;
-    })));
+    const handlePageChange = (event, value) => {
+        setPage(value);
+    };
 
+    const handlePageSizeChange = (event) => {
+        setPageSize(event.target.value);
+        setPage(1);
+    };
 
-    // TODO: List 어떻게 표현할꺼임?
     return (
         <CssVarsProvider disableTransitionOnChange>
             <CssBaseline />
@@ -54,56 +101,62 @@ export default function MealKits(){
                     <HeaderSection />
                     <Search />
                     <Filters />
-                    <Stack spacing={2} sx={{ overflow: 'auto' }}>
-                        {/*{Object.entries(mealKitsList).map((entire => {
-                            return <RentalCard
-                                title={entire.}
+                    {mealKitsList.map(value =>
+                        <Stack spacing={2} sx={{ overflow: 'auto' }} key={value.id}>
+                            <RentalCard
+                                title={value.mname}
+                                category={value.mcategory}
+                                rareFind
+                                image="https://images.unsplash.com/photo-1568605114967-8130f3a36994?auto=format&fit=crop&w=400" // TODO: image는 따로 저장해야할 듯! 새로 테이블을 만들자!
+                            />
+                            {/*<RentalCard
+                                title="ttt"
                                 category="Entire apartment rental in Collingwood"
                                 rareFind
                                 image="https://images.unsplash.com/photo-1568605114967-8130f3a36994?auto=format&fit=crop&w=400"
                             />
-                        }))}*/}
-                        <RentalCard
-                            title="ttt"
-                            category="Entire apartment rental in Collingwood"
-                            rareFind
-                            image="https://images.unsplash.com/photo-1568605114967-8130f3a36994?auto=format&fit=crop&w=400"
-                        />
-                        <RentalCard
-                            title="Designer NY style loft"
-                            category="Entire loft in central business district"
-                            liked
-                            image="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?auto=format&fit=crop&w=400"
-                        />
-                        <RentalCard
-                            title="5 minute walk from University of Melbourne"
-                            category="Entire rental unit in Carlton"
-                            image="https://images.unsplash.com/photo-1537726235470-8504e3beef77?auto=format&fit=crop&w=400"
-                        />
-                        <RentalCard
-                            title="Magnificent apartment next to public transport"
-                            category="Entire apartment rental in Collingwood"
-                            image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?auto=format&fit=crop&w=400"
-                        />
-                        <RentalCard
-                            title="Next to shoppng mall and public transport"
-                            category="Entire apartment rental in Collingwood"
-                            image="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?auto=format&fit=crop&w=400"
-                        />
-                        <RentalCard
-                            title="Endless ocean view"
-                            category="A private room in a shared apartment in Docklands"
-                            image="https://images.unsplash.com/photo-1564013799919-ab600027ffc6?auto=format&fit=crop&w=400"
-                        />
-                        <RentalCard
-                            title="A Stylish Apt, 5 min walk to Queen Victoria Market"
-                            category="one bedroom apartment in Collingwood"
-                            image="https://images.unsplash.com/photo-1481437156560-3205f6a55735?auto=format&fit=crop&w=400"
-                        />
-                    </Stack>
-                    <Pagination />
+                            <RentalCard
+                                title="Designer NY style loft"
+                                category="Entire loft in central business district"
+                                liked
+                                image="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?auto=format&fit=crop&w=400"
+                            />
+                            <RentalCard
+                                title="5 minute walk from University of Melbourne"
+                                category="Entire rental unit in Carlton"
+                                image="https://images.unsplash.com/photo-1537726235470-8504e3beef77?auto=format&fit=crop&w=400"
+                            />
+                            <RentalCard
+                                title="Magnificent apartment next to public transport"
+                                category="Entire apartment rental in Collingwood"
+                                image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?auto=format&fit=crop&w=400"
+                            />
+                            <RentalCard
+                                title="Next to shoppng mall and public transport"
+                                category="Entire apartment rental in Collingwood"
+                                image="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?auto=format&fit=crop&w=400"
+                            />
+                            <RentalCard
+                                title="Endless ocean view"
+                                category="A private room in a shared apartment in Docklands"
+                                image="https://images.unsplash.com/photo-1564013799919-ab600027ffc6?auto=format&fit=crop&w=400"
+                            />
+                            <RentalCard
+                                title="A Stylish Apt, 5 min walk to Queen Victoria Market"
+                                category="one bedroom apartment in Collingwood"
+                                image="https://images.unsplash.com/photo-1481437156560-3205f6a55735?auto=format&fit=crop&w=400"
+                            />*/}
+                        </Stack>
+                        )}
+                    <Pagination
+                        count={count}
+                        page={page}
+                        onChange={handlePageChange}
+                    />
                 </Stack>
             </Box>
         </CssVarsProvider>
     );
 }
+
+export default MealKits;
