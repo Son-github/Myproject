e156b2c47c91cc748743afd43363d504cbd4da27
jonginson <jongin718@gmail.com>
2023-12-16 00:05:19 +0900

    #7-이미지 불러오기 수정 및 검색 기능 수정
    Controller 파일 수정

--- project_a/src/main/java/com/son/project_a/controller/MealKitController.java
+++ project_a/src/main/java/com/son/project_a/controller/MealKitController.java
@@ -1,9 +1,13 @@
 package com.son.project_a.controller;
 
 import com.son.project_a.domain.MealKit;
+import com.son.project_a.domain.MealKitImage;
+import com.son.project_a.domain.constant.SearchType;
+import com.son.project_a.dto.MealKitDto;
 import com.son.project_a.exception.ResourceNotFoundException;
 import com.son.project_a.exception.ServerError;
 import com.son.project_a.repository.MealKitRepository;
+import com.son.project_a.response.MealKitResponse;
 import com.son.project_a.service.MealKitService;
 import com.son.project_a.service.PaginationService;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +17,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
@@ -44,16 +52,23 @@ public class MealKitController {
 
         try {
             List<MealKit> mealKits = new ArrayList<MealKit>();
+            List<MealKitImage> mealKitImages = new ArrayList<MealKitImage>();
             Pageable pagination = PageRequest.of(page, size);
             Page<MealKit> mealKitPage;
 
             if (searchValue == null) {
                 mealKitPage = mealKitRepository.findAll(pagination);
             } else {
-                mealKitPage = mealKitRepository.findBymName(searchValue, pagination);
+                mealKitPage = mealKitRepository.findMealKitBymNameContaining(searchValue, pagination);
             }
 
+            log.info("Page<MealKitDto> searchMealKits: {}", mealKitService.searchMealKits(searchValue, pagination));
+
             mealKits = mealKitPage.getContent();
+            //List<MealKit> mealKitStream = mealKits.stream().filter(mealKit -> mealKit.getMName().contains("e")).collect(Collectors.toList());
+            //List<String> mealKitImageStream = mealKitStream.stream().filter(mealKit -> mealKit.getMealKitComments());
+            //log.info("mealKitStream: {}", mealKitStream);
+            ///log.info("mealKits: {}", mealKits.get(0).getMealKitImages());
             Map<String, Object> response = new HashMap<String, Object>();
             response.put("mealKits", mealKits);
             response.put("totalPages", mealKitPage.getTotalPages());
