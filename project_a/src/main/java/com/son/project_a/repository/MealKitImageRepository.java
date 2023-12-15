792df71b4810c5c6fed8b94adf4683d7342af17d
jonginson <jongin718@gmail.com>
2023-12-16 00:05:40 +0900

    #7-이미지 불러오기 수정 및 검색 기능 수정
    Repository 파일 수정

--- project_a/src/main/java/com/son/project_a/repository/MealKitImageRepository.java
+++ project_a/src/main/java/com/son/project_a/repository/MealKitImageRepository.java
@@ -1,6 +1,7 @@
 package com.son.project_a.repository;
 
 import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.son.project_a.domain.MealKit;
 import com.son.project_a.domain.MealKitImage;
 import com.son.project_a.domain.QMealKitImage;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,17 +9,19 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
+import java.util.List;
+
 public interface MealKitImageRepository extends
         JpaRepository<MealKitImage, Long>,
         QuerydslPredicateExecutor<MealKitImage>,
         QuerydslBinderCustomizer<QMealKitImage> {
 
-    // todo: customize 한번 더 살펴보기!!
+    List<MealKitImage> findMealKitImageByMealKitId(Long id);
 
     @Override
     default void customize(QuerydslBindings bindings, QMealKitImage root){
         bindings.excludeUnlistedProperties(true);
         bindings.including(root.createdAt);
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);
-    };
+    }
 }
--- project_a/src/main/java/com/son/project_a/repository/MealKitRepository.java
+++ project_a/src/main/java/com/son/project_a/repository/MealKitRepository.java
@@ -2,7 +2,11 @@ package com.son.project_a.repository;
 
 import com.querydsl.core.types.dsl.StringExpression;
 import com.son.project_a.domain.MealKit;
+import com.son.project_a.domain.MealKitImage;
 import com.son.project_a.domain.QMealKit;
+import com.son.project_a.domain.QMealKitImage;
+import com.son.project_a.dto.MealKitDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,15 +19,17 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface MealKitRepository extends
         JpaRepository<MealKit, Long>,
         QuerydslPredicateExecutor<MealKit>,
-        QuerydslBinderCustomizer<QMealKit> {
+        QuerydslBinderCustomizer<QMealKit>
+{
 
 
-    Page<MealKit> findBymName(String mName, Pageable pageable);
+    Page<MealKit> findMealKitBymNameContaining(String mName, Pageable pageable);
+
 
     @Override
     default void customize(QuerydslBindings bindings, QMealKit root) {
         bindings.excludeUnlistedProperties(true);
-        bindings.including(root.mName, root.mCategory, root.mContent, root.mPrice);
+        bindings.including(root.mName, root.mCategory, root.mContent);
         bindings.bind(root.mName).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.mCategory).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.mContent).first(StringExpression::containsIgnoreCase);
